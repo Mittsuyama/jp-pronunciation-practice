497ab396225c612fb19ee3d1eef4d667489f3564
mittsuyama <1462973856@qq.com>
2019-09-10 15:48:18 +0800

    2019-9-9 练习

--- learn.py
+++ learn.py
@@ -55,7 +55,7 @@ class LearningWindow(QWidget):
             for x in range(1, int(self.repeat[key]) + 1):
                 sum += self.getExpValue(x, 5)
                 if sum >= 1:
-                    add += 2
+                    add += 3
                     sum = 0.0
             self.pd.loc[int(key), "weight"] = min(
                 100, int(self.pd.loc[int(key)]["weight"]) + add)
